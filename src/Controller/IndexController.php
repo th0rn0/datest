b0ac4000714b4983e07b6b0970833897b3aeaae6
th0rn0 <dev@th0rn0.co.uk>
2018-01-06 18:14:22 +0000

    Outcomes and primary market are now displayed

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace FootballInterface\Controller;
 
 use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController
 {
@@ -16,23 +17,59 @@ class IndexController
         $this->client = $client;
     }
 
-    public function index() {
+    public function index(Request $request) {
 
-        $response = $this->client->get(getenv('API_URL') . '/football/live');
-        $body = json_decode($response->getBody()->getContents());
+
+        $url = getenv('API_URL') . '/football/live?primaryMarkets=true';
+        
         //DEBUG
-        // dump($body->events);
+        // $primaryMarket = true;
+
+        // if ($request->query->get('markets')) {
+        //     $url = $url . '?primaryMarkets=true';
+        // }
+
+        $response = $this->client->get($url);
+        $body = json_decode($response->getBody()->getContents());
+
+
+        // Build array for view
+        foreach ($body->events as $event) {
+            $eventId = $event->eventId;
+            $return[$eventId]['name']                   = $event->name;
+            $return[$eventId]['displayOrder']           = $event->displayOrder;
+            $return[$eventId]['linkedEventTypeName']    = $event->linkedEventTypeName;
+            $return[$eventId]['scores']                 = $event->scores;
+            $return[$eventId]['competitors']            = $event->competitors;
+            $return[$eventId]['startTime']              = $event->startTime;
+            foreach ($body->markets as $markets) {
+                foreach ($markets as $market) {
+                    if ($market->eventId == $event->eventId) {
+                        $return[$eventId]['primaryMarket'] = $market;
+                    }
+                }
+            }
+            foreach ($body->outcomes as $marketId => $outcomes) {
+                if ($marketId == $return[$eventId]['primaryMarket']->marketId) {
+                    $return[$eventId]['outcomes'] = $outcomes;
+                }
+            }
+        }
+
+        //DEBUG
+        // dump($return);
+        // dump($body->outcomes);
         // die();
 
         // Sort the events display order
-        usort($body->events, function($a, $b)
+        usort($return, function($a, $b)
         {
-            return $a->displayOrder < $b->displayOrder;
+            return $a['displayOrder'] < $b['displayOrder'];
             //return strcmp($a->displayOrder, $b->displayOrder);
         });
 
         return $this->twig->render('index.html.twig', array(
-            'events' => $body->events
+            'events' => $return
         ));
 
         return json_encode($response->getBody()->getContents());
